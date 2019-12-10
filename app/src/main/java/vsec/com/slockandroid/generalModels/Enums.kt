ba2df305153f0729724f92df9a4bfefc3bb4f787
Rick Verstraten <0995396@hr.nl>
2019-12-10 16:20:19 +0100

    Added enumSet for disable button checks

--- app/src/main/java/vsec/com/slockandroid/generalModels/Enums.kt
+++ app/src/main/java/vsec/com/slockandroid/generalModels/Enums.kt
@@ -3,3 +3,16 @@ package vsec.com.slockandroid.generalModels
 enum class PasswordScore {
     WEAK, AVERAGE, STRONG, MARVELOUS
 }
+
+enum class ButtonState {
+    EMAIL_VALID,
+    EMAIL_EQUAL,
+    FIRST_NAME_VALID,
+    LAST_NAME_VALID,
+    PASSWORD_VALID,
+    PASSWORD_EQUAL,
+
+    FIRST_REGISTER_BUTTON_OK,
+    SECOND_REGISTER_BUTTON_OK,
+    LOGIN_BUTTON_OK
+}
