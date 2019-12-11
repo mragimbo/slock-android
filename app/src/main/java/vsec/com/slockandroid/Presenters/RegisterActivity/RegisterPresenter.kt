48981ecefcbb3473cf0ecef8a1e58c139351e8da
Aki Rahikkala <k8596@student.jamk.fi>
2019-12-11 14:37:21 +0100

    RegisterView data goes from step 1 to step 2 view

--- app/src/main/java/vsec/com/slockandroid/Presenters/RegisterActivity/RegisterPresenter.kt
+++ app/src/main/java/vsec/com/slockandroid/Presenters/RegisterActivity/RegisterPresenter.kt
@@ -5,6 +5,7 @@ import vsec.com.slockandroid.Controllers.Helpers
 import vsec.com.slockandroid.Presenters.HomeActivity.HomeView
 import vsec.com.slockandroid.generalModels.User
 
+
 class RegisterPresenter(private val view: View) {
 
     private val user: User
@@ -12,7 +13,28 @@ class RegisterPresenter(private val view: View) {
         user = User()
     }
 
+    fun assertEqual(element1: String, element2: String): Boolean = (element1 == element2)
+
+    fun checkEmailValidity(element: String): Boolean {
+        return Helpers.checkEmailIsValid(element)
+    }
+
+    fun checkNameField(element: String): Boolean {
+        val forbiddenChars: String = "1234567890" +
+                """!#¤%&/()=?{}<>|@£${"$"}€+~""" + "\"" + "\\\\"
+
+
+        if( element.matches( Regex(".*["+forbiddenChars+"].*") ) ) { return false }
+
+            return true
+        }
+
+
+    //fun checkPasswdValidity(element: String): Boolean {
+       // return Helpers.checkPasswordIsStrong(element)
+    //}
+
     interface View {
-        fun changeActivity(toActivity: Class<Activity>, extra: Map<String, String> = HashMap())
+        fun changeActivity(toActivity: Class<Activity>, extra: Map<String, String>)
     }
 }
--- app/src/main/java/vsec/com/slockandroid/Presenters/RegisterActivity/RegisterView.kt
+++ app/src/main/java/vsec/com/slockandroid/Presenters/RegisterActivity/RegisterView.kt
@@ -3,11 +3,17 @@ package vsec.com.slockandroid.Presenters.RegisterActivity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import kotlinx.android.synthetic.main.activity_register.*
 import vsec.com.slockandroid.R
 
 
 class RegisterView : Activity(), RegisterPresenter.View {
+
 
     private lateinit var presenter: RegisterPresenter
 
@@ -17,14 +23,82 @@ class RegisterView : Activity(), RegisterPresenter.View {
 
         this.presenter = RegisterPresenter(this)
 
+        //btn_next.isEnabled = false
+
+        var firstName: String = ""
+        var lastName: String = ""
+        var email: String = ""
+
         btn_next.setOnClickListener{
-            this.changeActivity(RegisterViewStep2::class.java as Class<Activity>)
+            var extras: MutableMap<String, String> = mutableMapOf()
+            extras.put("firstName", firstName)
+            extras.put("lastName", lastName)
+            extras.put("email", email)
+            this.changeActivity(RegisterViewStep2::class.java as Class<Activity>, extras)
         }
-    }
+
+
+        val t = Toast.makeText(this,"changed", LENGTH_LONG)
+
+        in_firstName.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                val bool = presenter.checkNameField(p0.toString())
+                t.setText(bool.toString())
+                t.show()
+                 firstName = p0.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        in_lastName.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                val bool = presenter.checkNameField(p0.toString())
+                t.setText(bool.toString())
+                t.show()
+                lastName = p0.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        in_reg_email.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                val bool = presenter.checkEmailValidity(p0.toString())
+                t.setText(bool.toString())
+                t.show()
+                email = p0.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        in_reg_conf_email.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                val bool = this@RegisterView.presenter.assertEqual(p0.toString().trim(),
+                    this@RegisterView.in_reg_email.text.toString().trim())
+                t.setText(bool.toString())
+                t.show()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        }
+
     override fun changeActivity(toActivity: Class<Activity>, extras: Map<String, String>) {
-        var intent: Intent = Intent(this, toActivity)
-        for(extra in extras){
-            intent.putExtra(extra.key, extra.value)
+        val intent: Intent = Intent(this, toActivity).apply{
+            for(extra in extras){
+                putExtra(extra.key, extra.value)
+            }
         }
         startActivity(intent)
     }
