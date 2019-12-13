b7320957c2e5420e6ef001e15458e16773c7467d
Rick Verstraten <0995396@hr.nl>
2019-12-13 13:19:20 +0100

    Bluetooth register call now works

--- app/src/main/java/vsec/com/slockandroid/Controllers/Callback/BluetoothGattConnectCallback.kt
+++ app/src/main/java/vsec/com/slockandroid/Controllers/Callback/BluetoothGattConnectCallback.kt
@@ -4,6 +4,11 @@ import android.bluetooth.*
 import android.content.ContentValues
 import android.util.Log
 import java.util.*
+import kotlin.collections.ArrayList
+
+private const val SERVICE_REGISTER_ID = "7c3e0e35-996f-4745-a62f-ecb0d6e971b2"
+private const val CHARACTERISTIC_REGISTER_NAME = "c3465381-d3fe-4234-bd2b-a642eaedb1fe"
+private const val CHARACTERISTIC_REGISTER_SECRET = "7c3e0e35-996f-4745-a62f-ecb0d6e971b2"
 
 
 object BluetoothGattConnectCallback: BluetoothGattCallback() {
@@ -14,6 +19,7 @@ object BluetoothGattConnectCallback: BluetoothGattCallback() {
     ) {
         when (newState) {
             BluetoothProfile.STATE_CONNECTED -> {
+                gatt.beginReliableWrite()
                 gatt.discoverServices()
             }
             BluetoothProfile.STATE_DISCONNECTED -> {
@@ -25,23 +31,50 @@ object BluetoothGattConnectCallback: BluetoothGattCallback() {
     override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
         Log.e("on services discoverd", status.toString())
         if (status == BluetoothGatt.GATT_SUCCESS) {
-            val service1: BluetoothGattService = gatt.getService(UUID.fromString("7c3e0e35-996f-4745-a62f-ecb0d6e971b2"))
-
-            var mWriteCharacteristic: BluetoothGattCharacteristic =
-                service1.getCharacteristic(UUID.fromString("c3465381-d3fe-4234-bd2b-a642eaedb1fe"))
-                mWriteCharacteristic.setValue("you should not see this, uuid")
-
-            if(gatt.writeCharacteristic(mWriteCharacteristic) == false){
-               Log.w("error", "Failed to write characteristic")
-            }
-
-            val service2 = gatt.getService(UUID.fromString("7c3e0e35-996f-4745-a62f-ecb0d6e971b2"))
-
-            var mWriteCharacteristic2 = service2.getCharacteristic(UUID.fromString("1f894374-6b00-4c13-9782-bfa63a479ed6"))
-            mWriteCharacteristic2.setValue("Lars you should see this, secret")
-            if(gatt.writeCharacteristic(mWriteCharacteristic2) == false){
-                Log.w("error2", "Hey, Lars you should not see this")
-            }
+            sendNameCharacteristic(gatt)
         }
+    }
+
+    private var characteristics: ArrayList<BluetoothGattCharacteristic?> = ArrayList()
+    override fun onCharacteristicWrite(
+        gatt: BluetoothGatt?,
+        characteristic: BluetoothGattCharacteristic?,
+        status: Int
+    ) {
+        characteristics.add(characteristic)
+        val hasName: Boolean = characteristics.find { it?.uuid == UUID.fromString(CHARACTERISTIC_REGISTER_NAME) } != null
+        val hasSecret: Boolean = characteristics.find { it?.uuid == UUID.fromString(CHARACTERISTIC_REGISTER_SECRET) } != null
+
+        if(hasName && hasSecret){
+            gatt?.executeReliableWrite()
+        }else if(hasName) {
+            sendSecretCharacteristic(gatt)
+        }else if(hasSecret){
+            sendNameCharacteristic(gatt)
+        }
+    }
+
+    fun sendNameCharacteristic(gatt: BluetoothGatt?){
+        if(gatt == null)
+            return
+
+        val registerservice: BluetoothGattService = gatt.getService(UUID.fromString(SERVICE_REGISTER_ID))
+
+        val registerNameCharacteristic: BluetoothGattCharacteristic = registerservice.getCharacteristic(UUID.fromString(
+            CHARACTERISTIC_REGISTER_NAME))
+        registerNameCharacteristic.setValue("uuid")
+
+        gatt.writeCharacteristic(registerNameCharacteristic)
+    }
+
+    fun sendSecretCharacteristic(gatt: BluetoothGatt?){
+        if(gatt == null)
+            return
+
+        val registerservice: BluetoothGattService = gatt.getService(UUID.fromString(SERVICE_REGISTER_ID))
+
+        val registerSecretCharacteristic: BluetoothGattCharacteristic = registerservice.getCharacteristic(UUID.fromString(CHARACTERISTIC_REGISTER_SECRET))
+        registerSecretCharacteristic.setValue("tokenseed")
+        gatt.writeCharacteristic(registerSecretCharacteristic)
     }
 }
