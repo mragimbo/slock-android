12afe664a5b83d5a69c1c568c6741130e656a3fe
Rick Verstraten <ricky-1999@live.nl>
2020-01-15 23:01:27 +0100

    WIP

--- app/src/main/java/vsec/com/slockandroid/Presenters/OwnedLocksActivity/OwnedLocksPresenter.kt
+++ app/src/main/java/vsec/com/slockandroid/Presenters/OwnedLocksActivity/OwnedLocksPresenter.kt
@@ -1,13 +1,53 @@
 package vsec.com.slockandroid.Presenters.OwnedLocksActivity
 
+import BluetoothCommandCallback
+import android.bluetooth.BluetoothDevice
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.parseList
+import vsec.com.slockandroid.Controllers.BluetoothController
+import vsec.com.slockandroid.Controllers.Callback.BluetoothScanCallback
+import vsec.com.slockandroid.Controllers.LockAuthController
 import vsec.com.slockandroid.generalModels.*
 
-class OwnedLocksPresenter(private val view: View){
-    fun executeCommand(lock: Lock, command: Int) {
+class OwnedLocksPresenter(override val view: _LocksOverviewPresenter.View): _LocksOverviewPresenter{
+    private var lockData: List<Lock> = emptyList()
 
+    private var lockAuthController: LockAuthController = LockAuthController(this)
+
+    override fun onScanDone(lock: Lock, command: String){
+        if(lock.getBleAddress() == null) {
+            this.view.toastLong("something went wrong")
+            return
+        }
+        var lockUuid: String = lock.getBleAddress() as String
+        val bleDevice: BluetoothDevice? = BluetoothScanCallback.scannedBleDevices.find { it.address == lockUuid }
+        bleDevice?.connectGatt(BluetoothController.context,false, BluetoothCommandCallback(lock, command, ::onNotification))
     }
 
-    interface View {
-        fun <T> changeActivity(toActivity: Class<T>, extras: Map<String, String>)
+    @ImplicitReflectionSerializer
+    override fun setLocks(locksJson: String) {
+        this.lockData = Json.parseList(locksJson)
+        this.view.refreshList(this.lockData)
+    }
+
+    fun fetchAccessibleLocks() {
+        this.lockAuthController.executeGetLocks()
+    }
+
+    fun executeCommand(lock: Lock, command: Int) {
+        this.lockAuthController.executeLockCommand(lock,command)
+    }
+
+    private fun onNotification(lock: Lock, status: String){
+        if (status.startsWith("200")){
+            if(lock.getId() != null){
+                this.lockAuthController.executeRatchetTick(lock.getId() as Int)
+            }
+            //ratchetTickTask.execute(lock.getId())
+            //ratchetTickTask = RatchetTickTask(this.view)
+        }else{
+            //do sync call
+        }
     }
 }
--- app/src/main/java/vsec/com/slockandroid/Presenters/OwnedLocksActivity/OwnedLocksView.kt
+++ app/src/main/java/vsec/com/slockandroid/Presenters/OwnedLocksActivity/OwnedLocksView.kt
@@ -4,19 +4,30 @@ import android.app.Activity
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_accessible_locks.*
 
 import kotlinx.android.synthetic.main.activity_owned_locks.*
+import vsec.com.slockandroid.Presenters.AccessibleLocksActivity.AccessibleLocksRecyclerAdapter
 
 import vsec.com.slockandroid.Presenters.RegisterLockActivity.RegisterLockView
 import vsec.com.slockandroid.Presenters.RegisterLockActivity.RegisterLockPresenter
 import vsec.com.slockandroid.R
+import vsec.com.slockandroid.generalModels.Lock
+import vsec.com.slockandroid.generalModels._LocksOverviewPresenter
 
-class OwnedLocksView : Activity(), OwnedLocksPresenter.View {
+class OwnedLocksView : Activity(), _LocksOverviewPresenter.View {
+    private lateinit var presenter: OwnedLocksPresenter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_owned_locks)
+        presenter = OwnedLocksPresenter(this)
 
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
 
         btn_add_lock.setOnClickListener {
             var extras: MutableMap<String, String> = mutableMapOf()
@@ -27,11 +38,20 @@ class OwnedLocksView : Activity(), OwnedLocksPresenter.View {
     }
 
     override fun <T> changeActivity(toActivity: Class<T>, extras: Map<String, String>) {
-        val intent: Intent = Intent(this, toActivity).apply{
-            for(e in extras){
+        val intent: Intent = Intent(this, toActivity).apply {
+            for (e in extras) {
                 putExtra(e.key, e.value)
             }
         }
         startActivity(intent)
+    }
+
+    override fun toastLong(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun refreshList(locks: List<Lock>) {
+        val accLockAdapter = OwnedLocksRecyclerAdapter(this.presenter,locks)
+        recyclerView.adapter = accLockAdapter
     }
 }
